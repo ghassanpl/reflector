157a913c01b294eaaa9aefe2b07a4cb7ddd04199
kronikarz <kronikarz@gmail.com>
2019-07-14 22:09:12 +0200

    * yay most things work (?)

--- test.cpp
+++ test.cpp
@@ -1,15 +1,27 @@
-
 #include "Component.h"
+#include <fmt/format.h>
 
-struct Visitor
+struct FieldVisitor
 {
 	template <typename T>
-	void operator()(const char* name, T&& pointer, const Reflector::FieldReflectionData& (*field_reflection_getter)(), std::true_type can_edit)
+	void operator()(const Reflector::FieldReflectionData* data, T&& pointer, std::true_type can_edit)
 	{
+		fmt::print("Field: {}\n", data->Name);
 	}
+
 	template <typename T>
-	void operator()(const char* name, T&& pointer, const Reflector::FieldReflectionData& (*field_reflection_getter)(), std::false_type cant_edit)
+	void operator()(const Reflector::FieldReflectionData* data, T&& pointer, std::false_type cant_edit)
 	{
+		fmt::print("Field: {}\n", data->Name);
+	}
+}; 
+
+struct MethodVisitor
+{
+	template <typename T, typename U>
+	void operator()(const Reflector::MethodReflectionData* data, T&& pointer, U&& ptr)
+	{
+		fmt::print("Method: {} ({})\n", data->Name, typeid(U).name());
 	}
 };
 
@@ -17,6 +29,7 @@ int main()
 {
 	Ass::Component component;
 
-	Ass::Component::StaticVisitFields(Visitor{});
-
+	Ass::Component::StaticVisitFields(FieldVisitor{});
+	Ass::Component::StaticVisitMethods(MethodVisitor{});
+	return 0;
 }
